c09ebc29fef3a26845e714974655a0a691849d3e
Sijita <simonjt.s2001@gmail.com>
2023-01-16 12:24:56 -0500

    Website refactoring
    website refactoring with best practices

--- src/components/PokemonCardExtended.jsx
+++ src/components/PokemonCardExtended.jsx
@@ -1,6 +1,10 @@
 import useSWR from "swr";
 import { useGlobalContext } from "../hooks/useGlobalContext";
 import { useParams } from "react-router-dom";
+import {
+  typeTextColor,
+  typeBackgroundColor,
+} from "../constants/pokemonTypeColor";
 
 export default function PokemonCardExtended() {
   const { name } = useParams();
@@ -14,43 +18,7 @@ export default function PokemonCardExtended() {
         <div className="flex flex-col sm:flex-row items-center gap-10">
           <h2
             className={`order-last sm:order-first text-5xl capitalize text-center font-bold ${
-              pokemon.types[0].type.name == "grass"
-                ? "text-green-500"
-                : pokemon.types[0].type.name == "water"
-                ? "text-blue-500"
-                : pokemon.types[0].type.name == "fire"
-                ? "text-red-500"
-                : pokemon.types[0].type.name == "bug"
-                ? "text-lime-700"
-                : pokemon.types[0].type.name == "poison"
-                ? "text-purple-500"
-                : pokemon.types[0].type.name == "normal"
-                ? "text-gray-500"
-                : pokemon.types[0].type.name == "electric"
-                ? "text-yellow-300"
-                : pokemon.types[0].type.name == "ground"
-                ? "text-yellow-700"
-                : pokemon.types[0].type.name == "fairy"
-                ? "text-pink-500"
-                : pokemon.types[0].type.name == "fighting"
-                ? "text-red-700"
-                : pokemon.types[0].type.name == "psychic"
-                ? "text-pink-300"
-                : pokemon.types[0].type.name == "rock"
-                ? "text-gray-700"
-                : pokemon.types[0].type.name == "ghost"
-                ? "text-purple-300"
-                : pokemon.types[0].type.name == "ice"
-                ? "text-blue-300"
-                : pokemon.types[0].type.name == "dragon"
-                ? "text-purple-700"
-                : pokemon.types[0].type.name == "flying"
-                ? "text-blue-700"
-                : pokemon.types[0].type.name == "dark"
-                ? "text-gray-900"
-                : pokemon.types[0].type.name == "steel"
-                ? "text-gray-300"
-                : "gray-500"
+              typeTextColor[pokemon?.types[0].type.name]
             }`}
           >
             {pokemon.name}
@@ -62,43 +30,7 @@ export default function PokemonCardExtended() {
         <div className="flex items-center gap-5">
           <span
             className={`badge badge-lg ${
-              pokemon.types[0].type.name == "grass"
-                ? "bg-green-500"
-                : pokemon.types[0].type.name == "water"
-                ? "bg-blue-500"
-                : pokemon.types[0].type.name == "fire"
-                ? "bg-red-500"
-                : pokemon.types[0].type.name == "bug"
-                ? "bg-lime-700"
-                : pokemon.types[0].type.name == "poison"
-                ? "bg-purple-500"
-                : pokemon.types[0].type.name == "normal"
-                ? "bg-gray-500"
-                : pokemon.types[0].type.name == "electric"
-                ? "bg-yellow-300"
-                : pokemon.types[0].type.name == "ground"
-                ? "bg-yellow-700"
-                : pokemon.types[0].type.name == "fairy"
-                ? "bg-pink-500"
-                : pokemon.types[0].type.name == "fighting"
-                ? "bg-red-700"
-                : pokemon.types[0].type.name == "psychic"
-                ? "bg-pink-300"
-                : pokemon.types[0].type.name == "rock"
-                ? "bg-gray-700"
-                : pokemon.types[0].type.name == "ghost"
-                ? "bg-purple-300"
-                : pokemon.types[0].type.name == "ice"
-                ? "bg-blue-300"
-                : pokemon.types[0].type.name == "dragon"
-                ? "bg-purple-700"
-                : pokemon.types[0].type.name == "flying"
-                ? "bg-blue-700"
-                : pokemon.types[0].type.name == "dark"
-                ? "bg-gray-900"
-                : pokemon.types[0].type.name == "steel"
-                ? "bg-gray-300"
-                : "bg-gray-500"
+              typeBackgroundColor[pokemon?.types[0].type.name]
             } ${
               pokemon.types[0].type.name == "electric" ||
               pokemon.types[0].type.name == "steel" ||
@@ -119,43 +51,7 @@ export default function PokemonCardExtended() {
           {pokemon.types[1]?.type.name && (
             <span
               className={`badge badge-lg ${
-                pokemon.types[1]?.type.name == "grass"
-                  ? "bg-green-500"
-                  : pokemon.types[1]?.type.name == "water"
-                  ? "bg-blue-500"
-                  : pokemon.types[1]?.type.name == "fire"
-                  ? "bg-red-500"
-                  : pokemon.types[1]?.type.name == "bug"
-                  ? "bg-lime-700"
-                  : pokemon.types[1]?.type.name == "poison"
-                  ? "bg-purple-500"
-                  : pokemon.types[1]?.type.name == "normal"
-                  ? "bg-gray-500"
-                  : pokemon.types[1]?.type.name == "electric"
-                  ? "bg-yellow-300"
-                  : pokemon.types[1]?.type.name == "ground"
-                  ? "bg-yellow-700"
-                  : pokemon.types[1]?.type.name == "fairy"
-                  ? "bg-pink-500"
-                  : pokemon.types[1]?.type.name == "fighting"
-                  ? "bg-red-700"
-                  : pokemon.types[1]?.type.name == "psychic"
-                  ? "bg-pink-300"
-                  : pokemon.types[1]?.type.name == "rock"
-                  ? "bg-gray-700"
-                  : pokemon.types[1]?.type.name == "ghost"
-                  ? "bg-purple-300"
-                  : pokemon.types[1]?.type.name == "ice"
-                  ? "bg-blue-300"
-                  : pokemon.types[1]?.type.name == "dragon"
-                  ? "bg-purple-700"
-                  : pokemon.types[1]?.type.name == "flying"
-                  ? "bg-blue-700"
-                  : pokemon.types[1]?.type.name == "dark"
-                  ? "bg-gray-900"
-                  : pokemon.types[1]?.type.name == "steel"
-                  ? "bg-gray-300"
-                  : "bg-gray-500"
+                typeBackgroundColor[pokemon?.types[1].type.name]
               } ${
                 pokemon.types[0].type.name == "electric" ||
                 pokemon.types[0].type.name == "steel" ||
@@ -218,43 +114,7 @@ export default function PokemonCardExtended() {
         src={pokemon.sprites.other.home.front_default}
         alt={pokemon.name}
         className={`w-4/5 sm:w-80 sm:h-80 mx-auto order-first ${
-          pokemon.types[0].type.name == "grass"
-            ? "bg-green-500"
-            : pokemon.types[0].type.name == "water"
-            ? "bg-blue-500"
-            : pokemon.types[0].type.name == "fire"
-            ? "bg-red-500"
-            : pokemon.types[0].type.name == "bug"
-            ? "bg-lime-700"
-            : pokemon.types[0].type.name == "poison"
-            ? "bg-purple-500"
-            : pokemon.types[0].type.name == "normal"
-            ? "bg-gray-500"
-            : pokemon.types[0].type.name == "electric"
-            ? "bg-yellow-300"
-            : pokemon.types[0].type.name == "ground"
-            ? "bg-yellow-700"
-            : pokemon.types[0].type.name == "fairy"
-            ? "bg-pink-500"
-            : pokemon.types[0].type.name == "fighting"
-            ? "bg-red-700"
-            : pokemon.types[0].type.name == "psychic"
-            ? "bg-pink-300"
-            : pokemon.types[0].type.name == "rock"
-            ? "bg-gray-700"
-            : pokemon.types[0].type.name == "ghost"
-            ? "bg-purple-300"
-            : pokemon.types[0].type.name == "ice"
-            ? "bg-blue-300"
-            : pokemon.types[0].type.name == "dragon"
-            ? "bg-purple-700"
-            : pokemon.types[0].type.name == "flying"
-            ? "bg-blue-700"
-            : pokemon.types[0].type.name == "dark"
-            ? "bg-gray-900"
-            : pokemon.types[0].type.name == "steel"
-            ? "bg-gray-300"
-            : "gray-500"
+          typeBackgroundColor[pokemon?.types[0].type.name]
         } rounded-full`}
       />
     </div>
--- src/components/PokemonsGrid.jsx
+++ src/components/PokemonsGrid.jsx
@@ -1,6 +1,9 @@
 import { useGlobalContext } from "../hooks/useGlobalContext";
 import PokemonsCard from "./PokemonsCard";
 import InfiniteScroll from "react-infinite-scroller";
+import {
+  typeBackgroundColor,
+} from "../constants/pokemonTypeColor";
 
 export default function PokemonsGrid() {
   const { singlePokemon, search, pokemons, setOffsetPageNumber } =
@@ -36,101 +39,11 @@ export default function PokemonsGrid() {
               image={pokemon.sprites.front_default}
               type={pokemon.types[0].type.name}
               secondaryType={pokemon.types[1]?.type.name}
-              secondaryColor={
-                pokemon.types[1]?.type.name == "grass"
-                  ? "bg-green-500"
-                  : pokemon.types[1]?.type.name == "water"
-                  ? "bg-blue-500"
-                  : pokemon.types[1]?.type.name == "fire"
-                  ? "bg-red-500"
-                  : pokemon.types[1]?.type.name == "bug"
-                  ? "bg-lime-700"
-                  : pokemon.types[1]?.type.name == "poison"
-                  ? "bg-purple-500"
-                  : pokemon.types[1]?.type.name == "normal"
-                  ? "bg-gray-500"
-                  : pokemon.types[1]?.type.name == "electric"
-                  ? "bg-yellow-300"
-                  : pokemon.types[1]?.type.name == "ground"
-                  ? "bg-yellow-700"
-                  : pokemon.types[1]?.type.name == "fairy"
-                  ? "bg-pink-500"
-                  : pokemon.types[1]?.type.name == "fighting"
-                  ? "bg-red-700"
-                  : pokemon.types[1]?.type.name == "psychic"
-                  ? "bg-pink-300"
-                  : pokemon.types[1]?.type.name == "rock"
-                  ? "bg-gray-700"
-                  : pokemon.types[1]?.type.name == "ghost"
-                  ? "bg-purple-300"
-                  : pokemon.types[1]?.type.name == "ice"
-                  ? "bg-blue-300"
-                  : pokemon.types[1]?.type.name == "dragon"
-                  ? "bg-purple-700"
-                  : pokemon.types[1]?.type.name == "flying"
-                  ? "bg-blue-700"
-                  : pokemon.types[1]?.type.name == "dark"
-                  ? "bg-gray-900"
-                  : pokemon.types[1]?.type.name == "steel"
-                  ? "bg-gray-300"
-                  : "bg-gray-500"
-              }
+              secondaryColor={typeBackgroundColor[pokemon.types[1]?.type.name]}
               color={
-                pokemon.types[0].type.name == "grass" ||
-                pokemon.types[1]?.type.name == "grass"
-                  ? "bg-green-500"
-                  : pokemon.types[0].type.name == "water" ||
-                    pokemon.types[1]?.type.name == "water"
-                  ? "bg-blue-500"
-                  : pokemon.types[0].type.name == "fire" ||
-                    pokemon.types[1]?.type.name == "fire"
-                  ? "bg-red-500"
-                  : pokemon.types[0].type.name == "bug" ||
-                    pokemon.types[1]?.type.name == "bug"
-                  ? "bg-lime-700"
-                  : pokemon.types[0].type.name == "poison" ||
-                    pokemon.types[1]?.type.name == "poison"
-                  ? "bg-purple-500"
-                  : pokemon.types[0].type.name == "normal" ||
-                    pokemon.types[1]?.type.name == "normal"
-                  ? "bg-gray-500"
-                  : pokemon.types[0].type.name == "electric" ||
-                    pokemon.types[1]?.type.name == "electronic"
-                  ? "bg-yellow-300"
-                  : pokemon.types[0].type.name == "ground" ||
-                    pokemon.types[1]?.type.name == "ground"
-                  ? "bg-yellow-700"
-                  : pokemon.types[0].type.name == "fairy" ||
-                    pokemon.types[1]?.type.name == "fairy"
-                  ? "bg-pink-500"
-                  : pokemon.types[0].type.name == "fighting" ||
-                    pokemon.types[1]?.type.name == "fighting"
-                  ? "bg-red-700"
-                  : pokemon.types[0].type.name == "psychic" ||
-                    pokemon.types[1]?.type.name == "psychic"
-                  ? "bg-pink-300"
-                  : pokemon.types[0].type.name == "rock" ||
-                    pokemon.types[1]?.type.name == "rock"
-                  ? "bg-gray-700"
-                  : pokemon.types[0].type.name == "ghost" ||
-                    pokemon.types[1]?.type.name == "ghost"
-                  ? "bg-purple-300"
-                  : pokemon.types[0].type.name == "ice" ||
-                    pokemon.types[1]?.type.name == "ice"
-                  ? "bg-blue-300"
-                  : pokemon.types[0].type.name == "dragon" ||
-                    pokemon.types[1]?.type.name == "dragon"
-                  ? "bg-purple-700"
-                  : pokemon.types[0].type.name == "flying" ||
-                    pokemon.types[1]?.type.name == "flying"
-                  ? "bg-blue-700"
-                  : pokemon.types[0].type.name == "dark" ||
-                    pokemon.types[1]?.type.name == "dark"
-                  ? "bg-gray-900"
-                  : pokemon.types[0].type.name == "steel" ||
-                    pokemon.types[1]?.type.name == "steel"
-                  ? "bg-gray-300"
-                  : "bg-gray-500"
+                typeBackgroundColor[
+                  pokemon.types[0].type.name ?? pokemon.types[1].type.name
+                ]
               }
               textColor={
                 pokemon.types[0].type.name == "electric" ||
